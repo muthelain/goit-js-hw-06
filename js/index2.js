f646627f96f3999f12aff3573069eb0e2522cc47
Kirill <stevemcc.man@gmail.com>
2023-04-09 22:49:48 +0300

    Update index2.js

--- js/index2.js
+++ js/index2.js
@@ -1,14 +1,18 @@
 // JavaScript містить масив рядків.
 
-const ingredients = ["Potatoes","Mushrooms","Garlic","Tomatos","Herbs","Condiments",];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
 
 const list = document.querySelector('#ingredients');
+const fragment = document.createDocumentFragment(); // створюємо DocumentFragment
+
 ingredients.forEach(value => {
     const listItem = document.createElement('li');
     listItem.textContent = value;
-    list.append(listItem);
     listItem.classList.add("item");
-})
+    fragment.appendChild(listItem); // додаємо кожен елемент в DocumentFragment замість прямої вставки в DOM
+});
+
+list.appendChild(fragment); 
 
 // Напиши скрипт, який для кожного елемента масиву ingredients:
 
